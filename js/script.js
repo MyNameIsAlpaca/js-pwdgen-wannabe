482f4f271c0c083b10f8cb3d925f68a9de9657f0
MyNameIsAlpaca <Saragosagabriele@gmail.com>
2023-02-21 18:27:52 +0100

    Done

---
+++ js/script.js
@@ -0,0 +1,7 @@
+let userName = prompt("Qual è il tuo nome?");
+
+let userSurname = prompt("Qual è il tuo cognome?");
+
+let userColor = prompt("Qual è il tuo colore preferito?");
+
+document.writeln(userName + userSurname + userColor + 69);
